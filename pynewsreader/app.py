e5a53a06977dd21f0c5371e28a179cc5ac4931a6
radinplaid <franey.mark@gmail.com>
2024-12-29 18:08:06 -0500

    Switch to fasthtml for web app

--- pynewsreader/app.py
+++ pynewsreader/app.py
@@ -1,169 +1,167 @@
-# These are all we need for our purposes
-import datetime
-import random
+import inspect
 
-import fire
-import pytz
-from dateutil.parser import parse as date_parse
-from flask import Flask, jsonify, render_template, request
+from fa6_icons import svgs
+from fasthtml.common import *
+from fire import Fire
 
-from pynewsreader.core import PyNewsReader
+from .core import PyNewsReader
 
-app = Flask(__name__)
+pnr = PyNewsReader()
 
-
-@app.route("/")
-def index():
-    return render_template("index.html", **{
-        "news": []
-    })
-
-
-@app.route('/update', methods=['GET'])
-def update():
-    pnr = PyNewsReader()
-    pnr.update()
-    return jsonify(True)
+app, rt = fast_app(
+    hdrs=(
+        Script(
+            src="https://cdnjs.cloudflare.com/ajax/libs/flexboxgrid/6.3.1/flexboxgrid.min.css",
+            type="text/css",
+        )
+    )
+)
 
 
-@app.route('/mark_important', methods=['GET'])
-def mark_important():
-    pnr = PyNewsReader()
-    args = request.args.to_dict()
-    if 'link' in args:
-        pnr._mark_entries_important_by_url(args['link'])
-        return jsonify(True)
+def b64_enc(x):
+    return base64.b64encode(x.encode("ascii")).decode("ascii")
+
+
+def b64_dec(x):
+    return base64.b64decode((x.encode("ascii"))).decode("ascii")
+
+
+def article_card(entry):
+
+    return Card(
+        # P("Author: " + entry.author if entry.author else ""),
+        P("Source: " + entry.feed.title),
+        Button(
+            "👍",
+            cls="secondary",
+            hx_post=f"/mark_important/{b64_enc(entry.feed.url)}/{b64_enc(entry.id)}",
+            hx_swap="beforeend",
+        ),
+        Nbsp(),
+        Button(
+            "👎",
+            cls="secondary",
+            hx_post=f"/mark_unimportant/{b64_enc(entry.feed.url)}/{b64_enc(entry.id)}",
+            hx_swap="beforeend",
+        ),
+        P("❗Important") if entry.important else (),
+        header=A(
+            Img(
+                style="max-width:64px; max-height: 64px; min-width:64px; max-width: 64px; margin-right: 20px;",
+                src=f"https://t1.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&size=128&url={entry.feed.url}",
+            ),
+            H3("❗" + entry.title) if entry.important else H4(entry.title),
+            style="display: flex; text-decoration: none; ",
+            cls="row",
+            href=entry.link,
+            target="_blank",
+        ),
+        footer="Date: " + get_date(entry),
+        style="min-width: 450px; max-width:450px;",
+    )
+
+
+def get_date(entry):
+    if entry.published is not None:
+        article_date = entry.published
     else:
-        return jsonify(False)
+        article_date = entry.added
+    return article_date.strftime("%Y-%m-%d")
 
 
-@app.route('/mark_unimportant', methods=['GET'])
-def mark_unimportant():
-    pnr = PyNewsReader()
-    args = request.args.to_dict()
-    if 'link' in args:
-        pnr._mark_entries_unimportant_by_url(args['link'])
-        return jsonify(True)
-    else:
-        return jsonify(False)
+def show_articles(mark_read: bool = True, limit: int = 12):
+    entries = pnr._get_entries(limit=limit, read=False)
+    entries = [i for i in entries]
+
+    if mark_read:
+        for entry in entries:
+            pnr._reader.mark_entry_as_read(entry)
+
+    return Div(
+        Div(
+            (article_card(entry) for entry in entries),
+            style="""display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  flex-wrap: wrap;""",
+        ),
+        cls="row",
+        id="main",
+    )
 
 
-@app.route('/add_tag', methods=['GET'])
-def add_tag():
-    pnr = PyNewsReader()
-    args = request.args.to_dict()
-    if 'link' in args:
-        pnr._add_tag(args['link'])
-        return jsonify(True)
-    else:
-        return jsonify(False)
+@rt("/")
+def get():
+    return Div(
+        Nav(
+            Ul(Li(H1("pynewsreader"))),
+            Ul(
+                Li(
+                    A(
+                        Svg(svgs.github.brands, width=40, height=40),
+                        href="https://github.com/radinplaid/pynewsreader",
+                        target="_blank",
+                    ),
+                )
+            ),
+        ),
+        show_articles(),
+        Span(cls="mdi mdi-github-circle"),
+        Button("Next", hx_get="/change", hx_target="#main", hx_swap="innerHTML"),
+        style="padding-left: 10px;padding-right: 10px",
+    )
 
 
-@app.route('/remove_tag', methods=['GET'])
-def remove_tag():
-    pnr = PyNewsReader()
-    args = request.args.to_dict()
-    if 'link' in args:
-        pnr._remove_tag(args['link'])
-        return jsonify(True)
-    else:
-        return jsonify(False)
+@rt("/change")
+def get():
+    return Div(show_articles())
 
 
-@app.route('/feeds', methods=['GET'])
-def feeds():
-    pnr = PyNewsReader()
-    return jsonify([i[1] for i in pnr.feeds()])
+@rt("/mark_important")
+@app.post("/mark_important/{feed_url}/{id}")
+def get(id: str, feed_url: str):
+    id = b64_dec(id)
+    feed_url = b64_dec(feed_url)
+    return pnr._mark_important(feed_url=feed_url, entry_id=id)
 
 
-@app.route('/getnews', methods=['GET'])
-def getnews():
-    pnr = PyNewsReader()
-    news = []
-    all_links = []
-    only_unread = False
-    only_important = None
-
-    args = request.args.to_dict()
-
-    if 'limit' in args:
-        LIMIT = int(args['limit'])
-    else:
-        LIMIT = 50
-
-    if 'important' in args:
-        if args['important'] == 'false':
-            only_important = None
-        else:
-            only_important = True
-
-    if 'search' in args:
-        if args['unread'] == 'false':
-            only_unread = None
-
-    if 'search' in args and args['search'] != 'null' and len(args['search']) > 0:
-        if args['search'] != "null" and args['search'] != "":
-            dat = [pnr._reader.get_entry(
-                i) for i in pnr._reader.search_entries(args['search'], sort='recent')]
-        else:
-            dat = pnr._get_entries(
-                limit=None, important=only_important, read=only_unread)
-    else:
-        dat = pnr._get_entries(
-            limit=None, important=only_important, read=only_unread)
-
-    if 'categories' in args:
-        selected_feeds = args['categories'].split(',')
-    else:
-        selected_feeds = [i[1] for i in pnr.feeds()]
-
-    for i in dat:
-        if (i.feed.url in pnr._feed_names and pnr._feed_names[i.feed.url] in selected_feeds) or i.feed.title in selected_feeds:
-            if i.published is not None:
-                article_date = i.published
-            else:
-                article_date = i.added
+@rt("/mark_unimportant")
+@app.post("/mark_unimportant/{feed_url}/{id}")
+def get(id: str, feed_url: str):
+    id = b64_dec(id)
+    feed_url = b64_dec(feed_url)
+    return pnr._mark_unimportant(feed_url=feed_url, entry_id=id)
 
 
-            if 'min_date' in args:
-                if (
-                    article_date < date_parse(args['min_date']).replace(tzinfo=pytz.UTC) or
-                    article_date > date_parse(args['max_date']).replace(
-                        tzinfo=pytz.UTC)+datetime.timedelta(days=1)
-                ):
-                    continue
+def main_app(
+    host: str = "localhost",
+    port: int = 8000,
+    reload: bool = True,
+    reload_includes: List[str] = [],
+    reload_excludes: List[str] = [],
+):
+    """pynewsreader web application launching script
 
-            pnr._reader.mark_entry_as_read(i)
-            if i.link not in all_links:
-                all_links.append(i.link)
-                news.append({
-                    "id": i.id,
-                    "title": i.title,
-                    "link": i.link,
-                    "published": str(article_date),
-                    "published_epoch": article_date.timestamp(),
-                    "source_url": i.feed_url,
-                    "source_name": i.feed.title,
-                    "icon": "mdi-bell" if i.important else "",
-                    "important": i.important,
-                    "tags": [{"key": j, "value": j} for j in pnr._get_tags(i)]
-                })
-            if len(news) >= LIMIT:
-                return jsonify(news)
-
-    # Return important news first
-    news = sorted(news, key = lambda x: x['important'], reverse=True)
-    
-    return jsonify(news)
+    Args:
+        host (str, optional): HTTP host. Defaults to "localhost".
+        port (int, optional): HTTP port. Defaults to 8000.
+        reload (bool, optional): Reload app on file changes. Defaults to True.
+        reload_includes (List[str], optional): Reload only if these files change. Defaults to [].
+        reload_excludes (List[str], optional): Do not reload if these files change. Defaults to [].
+    """
+    uvicorn.run(
+        f"pynewsreader.app:app",
+        host=host,
+        port=port,
+        reload=reload,
+        reload_includes=reload_includes,
+        reload_excludes=reload_excludes,
+    )
 
 
-def run_app(host="127.0.0.1", port=5000):
-    app.run(host=host, port=port)
-
-
-def main():
-    fire.Fire(run_app)
+def cli_app():
+    return Fire(main_app)
 
 
 if __name__ == "__main__":
-    main()
+    cli_app()
